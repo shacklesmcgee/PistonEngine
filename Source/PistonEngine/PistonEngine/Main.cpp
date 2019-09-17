899a94f3480701694d04ff6c67715252aef913c9
Ben <benmilette@gmail.com>
2019-09-17 13:08:45 -0400

    Test

---
+++ Source/PistonEngine/PistonEngine/Main.cpp
@@ -0,0 +1,19 @@
+//#include <>
+using namespace std;
+
+int main(void) 
+{
+
+	return 0;
+
+}
+
+DWORD ReadCPUSpeed() 
+{
+	DWORD BufSize = sizeof(DWORD);
+}
+//bool CheckMemory(const DWORDLONG physNeed, const DWORDLONG virtNeed)
+//{
+//	MEMORYSTATUSEX status;
+//
+//}
