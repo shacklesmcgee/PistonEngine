a3b650b02089d7c9fe91db7ae0fba912ca9e88ca
Ben <benmilette@gmail.com>
2019-09-17 21:20:00 -0400

    Adding CPU Arch Check

--- Source/PistonEngine/PistonEngine/Main.cpp
+++ Source/PistonEngine/PistonEngine/Main.cpp
@@ -1,15 +1,18 @@
 #include <Windows.h>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-bool CheckMemory(const DWORDLONG physNeed, const DWORDLONG virtNeed);
 DWORD ReadCPUSpeed();
+wstring ReadCPUArch();
 
 int main(void) 
 {
 
 	cout << "CPU Speed is : " << ReadCPUSpeed() << " MHz" << endl;
+	cout << "CPU Architechture is : " << ReadCPUArch(L"Identifier") << " MHz" << endl;
+
 
 	system("PAUSE");
 	return 0;
@@ -40,3 +43,42 @@ DWORD ReadCPUSpeed()
 	}
 	return dwMHz;
 }
+
+wstring ReadCPUArch(wstring name)
+{
+	DWORD BufSize = sizeof(DWORD);
+	//wstring name = "Identifier";
+
+	DWORD cbData;
+
+	DWORD type;
+	HKEY hKey;
+
+	if (RegQueryValueEx(hKey, name.c_str(), NULL, &type, NULL, &cbData) != ERROR_SUCCESS)
+	{
+		RegCloseKey(hKey);
+		throw "Could not read registry value";
+	}
+
+	if (type != REG_SZ)
+	{
+		RegCloseKey(hKey);
+		throw "Incorrect registry value type";
+	}
+
+	wstring value(cbData / sizeof(wchar_t), L'\0');
+	if (RegQueryValueEx(hKey, name.c_str(), NULL, NULL, reinterpret_cast<LPBYTE>(&value[0]), &cbData) != ERROR_SUCCESS)
+	{
+		RegCloseKey(hKey);
+		throw "Could not read registry value";
+	}
+
+	RegCloseKey(hKey);
+
+	size_t firstNull = value.find_first_of(L'\0');
+	if (firstNull != string::npos)
+		value.resize(firstNull);
+
+	return value;
+}
+
