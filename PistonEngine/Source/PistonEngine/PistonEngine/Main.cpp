332ebfb4ce77589fc9d75fb058301bb23c4d1fcc
Ben <benmilette@gmail.com>
2019-09-19 08:51:07 -0400

    Added Check Memory Feature

--- PistonEngine/Source/PistonEngine/PistonEngine/Main.cpp
+++ PistonEngine/Source/PistonEngine/PistonEngine/Main.cpp
@@ -11,6 +11,8 @@ using namespace std;
 bool isRunning = false;
 const float diskSpaceNeeded = 300;
 
+bool CheckMemory(const DWORDLONG physNeed, const DWORDLONG virtNeed);
+
 void CheckIsRunning()
 {
 	if (isRunning == true)
@@ -58,10 +60,54 @@ void CheckStorage(const float diskSpaceNeeded)
 	}
 }
 
+bool CheckMemory(const DWORDLONG physNeed, const DWORDLONG virtNeed)
+{
+	MEMORYSTATUSEX status;
+	status.dwLength = sizeof(status);
+	GlobalMemoryStatusEx(&status);
+
+	if (status.ullAvailPhys < (physNeed * (1024 * 1024)))
+	{
+		cout << "Not enough physical memory!" << endl;
+		return false;
+	}
+
+	if (status.ullAvailVirtual < (virtNeed * (1024 * 1024)))
+	{
+		cout << "Not enough virtual memory!" << endl;
+		return false;
+	}
+
+	cout << "Available Physical RAM : " << status.ullAvailPhys / (1024 * 1024) << " MB" << endl;
+	cout << "Available Virtual RAM : " << status.ullAvailVirtual / (1024 * 1024) << " MB" << endl;
+
+	return true;
+}
+
 int main()
 {
 	CheckIsRunning();
 	CheckStorage(diskSpaceNeeded);
+
+	//Check Physical RAM error
+	//if (CheckMemory(30000, 300))
+	//{
+	//	//Run Game
+	//}
+
+	//Check Virtual RAM error
+	//if (CheckMemory(300, 300000000))
+	//{
+	//	//Run Game
+	//}
+
+	//Check Available RAM
+	if (CheckMemory(300, 300))
+	{
+		//Run Game
+	}
+
+
 	system("PAUSE");
 	return 0;
 }
