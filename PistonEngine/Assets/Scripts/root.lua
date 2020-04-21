4691232a2bb744a101e85b1ea79b79bb20f21079
ReynoldsKTim <Reynolds.k.Timothy@gmail.com>
2020-04-21 01:14:22 -0400

    Added enemy waves
    Each wave contains only a single enemy

--- PistonEngine/Assets/Scripts/root.lua
+++ PistonEngine/Assets/Scripts/root.lua
@@ -1,6 +1,8 @@
 gameTimer = 125
 screenWidth = 512
 screenHeight = 512
+timeOfLastSpawn = 125
+numOfSpawns = 1
 
 function Start(args)
     height = GetTextHeight(Text)
@@ -12,6 +14,25 @@ function Update(dt)
 
     gameTimer = gameTimer - (dt/1)
     
+    if(gameTimer < timeOfLastSpawn - 9) then
+	timeOfLastSpawn = gameTimer
+
+	enemy = {
+                name = "enemy",
+    	        position = {x = 0, y = 32},
+	        rotation = {angle = 0.0, x = 0.0, y = 0.0}, 
+                scale = {x = 1.0, y = 1.0},
+	        graphics = {name = "enemy", texture = "../../Assets/Textures/enemy.png", animated = true, textureX = 0, textureY = 32, textureWidth = 32, textureHeight = 32, frameTime = 0.1, looping = true}, 
+	        script = {location = "../../Assets/Scripts/enemy.lua", args = {}},    
+	        rigidBody = {mass = 1.0, restitution = 1.0, obeysGravity = false}
+                }
+
+	--for i = 1, numOfSpawns do
+	    Create(GameObject, enemy)
+	--end
+	numOfSpawns = numOfSpawns + 1
+    end
+
     seconds = gameTimer % 60
     minutes = (gameTimer - seconds) / 60
 
